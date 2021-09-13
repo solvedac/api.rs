9c42b6e180619a5a9571df6e2b7139294e1787fe
RanolP <RanolP@users.noreply.github.com>
2021-09-13 23:28:25 +0000

    Follow-up solvedac/unofficial-documentation

--- src/apis/user_api.rs
+++ src/apis/user_api.rs
@@ -15,6 +15,13 @@ use crate::apis::ResponseContent;
 use super::{Error, configuration};
 
 
+/// struct for typed errors of method `get_user`
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum GetUserError {
+    UnknownValue(serde_json::Value),
+}
+
 /// struct for typed errors of method `get_user_problem_stats`
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -22,6 +29,34 @@ pub enum GetUserProblemStatsError {
     UnknownValue(serde_json::Value),
 }
 
+
+/// 사용자의 정보를 가져옵니다. 만약 로그인한 경우, 라이벌 여부도 가져옵니다.
+pub async fn get_user(configuration: &configuration::Configuration, handle: &str) -> Result<Vec<serde_json::Value>, Error<GetUserError>> {
+
+    let local_var_client = &configuration.client;
+
+    let local_var_uri_str = format!("{}/user/show", configuration.base_path);
+    let mut local_var_req_builder = local_var_client.request(reqwest::Method::GET, local_var_uri_str.as_str());
+
+    local_var_req_builder = local_var_req_builder.query(&[("handle", &handle.to_string())]);
+    if let Some(ref local_var_user_agent) = configuration.user_agent {
+        local_var_req_builder = local_var_req_builder.header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
+    }
+
+    let local_var_req = local_var_req_builder.build()?;
+    let local_var_resp = local_var_client.execute(local_var_req).await?;
+
+    let local_var_status = local_var_resp.status();
+    let local_var_content = local_var_resp.text().await?;
+
+    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+        serde_json::from_str(&local_var_content).map_err(Error::from)
+    } else {
+        let local_var_entity: Option<GetUserError> = serde_json::from_str(&local_var_content).ok();
+        let local_var_error = ResponseContent { status: local_var_status, content: local_var_content, entity: local_var_entity };
+        Err(Error::ResponseError(local_var_error))
+    }
+}
 
 /// 사용자가 푼 문제 개수를 문제 수준별로 가져옵니다.
 pub async fn get_user_problem_stats(configuration: &configuration::Configuration, handle: &str) -> Result<Vec<crate::models::InlineResponse2006>, Error<GetUserProblemStatsError>> {
